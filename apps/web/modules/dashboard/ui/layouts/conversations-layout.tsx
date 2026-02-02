ec69329d3a76d087405c80c19100666bb89dc0a1
mrPrecious2912 <quy_dev@bitify.xyz>
2026-02-02 15:29:10 +0700

    17: dashboard inbox

---
+++ apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
@@ -0,0 +1,22 @@
+import { ResizableHandle, ResizablePanel } from "@workspace/ui/components/resizable"
+
+import { ResizablePanelGroup } from "@workspace/ui/components/resizable"
+import { ConversationsPanel } from "../components/conversations-panel"
+
+export const ConversationsLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <ResizablePanelGroup className="h-full flex-1" orientation="horizontal">
+            <ResizablePanel defaultSize={100} maxSize={500} minSize={100}>
+                <ConversationsPanel />
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel className="h-full" defaultSize={70}>
+                {children}
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    )
+}
